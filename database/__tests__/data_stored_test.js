cd05c08ace9c9b273724d6d846100ff15caf8016
Richard Hai <hirichardhai@gmail.com>
2018-02-28 02:30:01 -0800

    grabbing and storing data in state works

--- database/__tests__/data_stored_test.js
+++ database/__tests__/data_stored_test.js
@@ -3,13 +3,8 @@ const db = require('../index');
 test('checks if reviews array has productIds 120', () => {
   let dataReview;
   db.getReviewsForProduct(120, (err, data) => {
-    console.log('this is the data', data);
+    dataReview = data;
   });
   console.log(dataReview);
-  expect(120).toBe(120);
-});
-
-
-db.getReviewsForProduct(200, (err, data) => {
-  console.log(data);
+  expect(dataReview[0].productId).toBe(120);
 });
