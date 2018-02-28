3dedeaf88a94279c0bdf573640d923223eb25269
Richard Hai <hirichardhai@gmail.com>
2018-02-28 01:09:45 -0800

    added ajax get for reviews from server

--- src/client/app/Reviews.jsx
+++ src/client/app/Reviews.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import Review from './Review.jsx';
+import $ from 'jquery';
 
 class Reviews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      reviews: [],
     };
+  }
+
+  fetch() {
+    $.ajax({
+      url: 'http://127.0.0.1:3004/v1/products/120/reviews',
+      method: 'GET',
+      success: (reviews) => {
+        console.log('successfully got review data');
+        reviews.forEach(review => this.state.reviews.push(review));
+      },
+      error: () => {
+        console.error('failed to fetch reviews');
+      },
+    });
   }
 
   render() {
