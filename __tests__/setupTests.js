9e9a81bfd3b62ce2fd129d6713aeb7a5bff8d90f
Richard Hai <hirichardhai@gmail.com>
2018-02-28 00:56:46 -0800

    creating tests environemtn

--- __tests__/setupTests.js
+++
@@ -1,4 +0,0 @@
-const Enzyme = require('enzyme');
-const EnzymeAdapter = require('enzyme-adapter-react-16');
-// This sets up the adapter to be used by Enzyme
-Enzyme.configure({ adapter: new EnzymeAdapter() });
