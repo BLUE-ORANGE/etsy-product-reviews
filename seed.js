a7fb73453e4165240c62e3dfacb7da58506370d4
hiRichardHai <hirichardhai@gmail.com>
2018-04-08 22:45:59 -0700

    attempting to fix more files for style guide

--- seed.js
+++ seed.js
@@ -17,6 +17,7 @@ const Review = sqlz.define(
     schema: 'public',
   },
 );
+
 const loremIpsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 const loremIpsumWords = loremIpsum.split(' ');
 const names = ['richard', 'ivy', 'andrew', 'trevor', 'gowri', 'zack', 'tom', 'hanyu', 'marlin', 'nick', 'joe', 'brian', 'eric', 'jordyn', 'james', 'zaid', 'wayne', 'henry', 'chase', 'moriah', 'anissa', 'andrea', 'austin', 'josh', 'jeffrey'];
